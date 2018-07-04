b38ac5f97d502fb5c63b96c2a44a418049fa0825
jjpps <joaopedro-ps@hotmail.com>
2018-07-03 23:05:59 -0300

    criada conexao com dialog flow
    principio do app

---
+++ api.js
@@ -0,0 +1,25 @@
+//
+var apiai = require('apiai');
+var app = apiai("85c44e1a5f374b8fa7cde6bc14d17023");
+
+
+// Function which returns speech from api.ai
+var getRes = function (query) {
+    var request = app.textRequest(query, {
+        sessionId: '<unique session id>'
+    });
+    const responseFromAPI = new Promise(
+        function (resolve, reject) {
+            request.on('error', function (error) {
+                reject(error);
+            });
+            request.on('response', function (response) {
+                resolve(response.result.fulfillment.speech);
+            });
+        });
+    request.end();
+    return responseFromAPI;
+};
+// test the command :
+getRes('ola').then(function (res) { console.log(res) });
+module.exports = { getRes }
